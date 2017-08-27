ccc662df86f9a6f3e5c3d3b6ae92c8aecbf4fa08
rasikakh <31240173+rasikakh@users.noreply.github.com>
2017-08-27 21:36:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('article-one', function(req,res){
+res.sendFile(path.join('page 1'));
+});
+
+app.get('article-two', function(req,res){
+res.sendFile(path.join('page 2'));
+});
+
+app.get('article-three', function(req,res){
+res.sendFile(path.join('page 3'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
